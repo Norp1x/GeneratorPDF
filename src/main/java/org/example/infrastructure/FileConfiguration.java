40b05511fd24aa44ae4aaa53fcb575f18d556be0
Norp1x <dudek.g@outlook.com>
2023-12-15 16:27:25 +0100

    fixed paths configuration, fixed bug "document not closed"

--- src/main/java/org/example/infrastructure/FileConfiguration.java
+++ src/main/java/org/example/infrastructure/FileConfiguration.java
@@ -1,14 +1,44 @@
 package org.example.infrastructure;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.io.File;
+
 @ConfigurationProperties(prefix = "pdf")
-public record FileConfiguration(
-        String fontPath,
-        String readPath,
-        String savePath,
-        String existingGeneratedPdfFilesPath
-) {
+public class FileConfiguration{
+    @Value("${pdf.fontPath}")
+    private String fontPath;
+
+    @Value("${pdf.readPath}")
+    private String readPath;
+
+    @Value("${pdf.savePath}")
+    private String savePath;
+
+    @Value("${pdf.existingGeneratedPdfFilesPath}")
+    private String existingGeneratedPdfFilesPath;
+
+    public String fontPath(){
+        return formatPath(fontPath);
+    }
+
+    public String readPath(){
+        return formatPath(readPath);
+    }
+
+    public String savePath(){
+        return formatPath(savePath);
+    }
+
+    public String existingGeneratedPdfFilesPath(){
+        return formatPath(existingGeneratedPdfFilesPath);
+    }
+
+    private String formatPath(String path){
+        return path.replace("/", File.separator);
+    }
+
 
 
 }
